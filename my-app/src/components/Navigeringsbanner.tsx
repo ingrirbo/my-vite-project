40c8e29185b703da674b351c3d8853a66650df54
Ingrid Borkenhagen <ingrid.rebo@hotmail.com>
2025-01-15 13:19:33 +0100

    utkast logo og navigasjonsbanner

---
+++ my-app/src/components/Navigeringsbanner.tsx
@@ -0,0 +1,13 @@
+export default function Navigeringsbanner() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Hjem</a></li>
+                <li><a href="/nyheter">Nyheter</a></li>
+                <li><a href="/salg">Salg</a></li>
+                <li><a href="/mobler">Møbler</a></li>
+                <li><a href="/interior">Interiør</a></li>
+            </ul>
+        </nav> 
+    )
+}
--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,7 +1,11 @@
+import Navigeringsbanner from "../components/Navigeringsbanner"
+import logo from "../images/logo.jpg"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+        <img src={logo} className="logo"/>
+        <Navigeringsbanner />
+    </>
   )
 } 
